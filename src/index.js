eae3f770062a2a704697bd575362e7572fec3d2b
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-07-17 01:17:46 -0300

    feat: todo existing verified

--- src/index.js
+++ src/index.js
@@ -73,6 +73,10 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 
   const todo = user.todos.find(todo => todo.id === id)
 
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found'})
+  }
+
   todo.title = title
   todo.deadline = deadline
 
